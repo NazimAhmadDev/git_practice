c85057312e1c1da757d06b893da949bc707a03bd
Nazim Ahmad <nazimahmedchd@gmail.com>
2025-06-05 22:39:19 +0530

    file added

---
+++ git-practice/src/main/java/org/example/gitpractice/Controller.java
@@ -0,0 +1,16 @@
+package org.example.gitpractice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping("/nazim")
+    public String myName() {
+        return "Nazim Ahmad";
+    }
+
+}
